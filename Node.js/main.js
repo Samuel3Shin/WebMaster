d396bbd4ee994f2fe4f9bcdc56c71c603ad781b4
Samuel3Shin <samuel3.shin@gmail.com>
2020-11-05 22:02:50 +0900

    home work little bit

--- Node.js/main.js
+++ Node.js/main.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url')
+var qs = require('querystring');
 
-function templateHTML(title, list, description,body) {
+function templateHTML(title, list, body) {
   var template = `<!doctype html>
   <html>
   <head>
@@ -12,6 +13,7 @@ function templateHTML(title, list, description,body) {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
@@ -30,7 +32,7 @@ function templateList(files) {
   return list;
 }
 
-var app = http.createServer(function(request,response){
+var app = http.createServer((request,response) => {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
@@ -45,7 +47,7 @@ var app = http.createServer(function(request,response){
           var description = "Hello, Node.js";
 
           var list = templateList(files);
-          var template = templateHTML(title, list, description, `<h2>${title}</h2>
+          var template = templateHTML(title, list, `<h2>${title}</h2>
           <p>${description}</p>`);
           response.writeHead(200);
           response.end(template);
@@ -57,7 +59,7 @@ var app = http.createServer(function(request,response){
           fs.readdir('./data', (err, files) => {
   
             var list = templateList(files);
-            var template = templateHTML(title, list, description, `<h2>${title}</h2>
+            var template = templateHTML(title, list, `<h2>${title}</h2>
             <p>${description}</p>`);
             response.writeHead(200);
             response.end(template);
@@ -65,14 +67,50 @@ var app = http.createServer(function(request,response){
           
         });
       }
+    } else if(pathname === '/create') {
+        fs.readdir('./data', (err, files) => {
+
+          var title = 'WEB - create';
+          var list = templateList(files);
+          var template = templateHTML(title, list,
+          ` <form action="http://localhost:3000/create_process" method="post">
+              <p>
+                  <input type="text" name="title" placeholder="title">
+              </p>
+              <p>
+                  <textarea name="description" placeholder="description"></textarea>
+              </p>
+              <p>
+                  <input type="submit">
+              </p>
+            </form>
+          `);
+          response.writeHead(200);
+          response.end(template);
+        });
+    } else if(pathname === '/create_process') {
+      var body = "";
+
+      request.on('data', (data) => {
+        body += data;
+      });
+      request.on('end', () => {
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        fs.writeFile(`data/${title}`, description, (error) => {
+          response.writeHead(302, {
+            "Location": `/?id=${title}`
+          });
+          response.end();
+        })
+      });
+
     } else {
       response.writeHead(404);
       response.end('Not found');
     }
 
-
-
- 
 });
 
 app.listen(3000);
